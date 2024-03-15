d43b8ce205067c753d4676425285391004bfaf31
sachin31344 <105095798+sachin31344@users.noreply.github.com>
2024-03-16 01:17:53 +0530

    Bulk call Done

--- server/config/database.js
+++ server/config/database.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connectdatabase = async () => {
   try {
+    console.log(process.env.MONGODB_CONNECT_URI);
     await mongoose.connect(process.env.MONGODB_CONNECT_URI);
     console.log("connected");
+    
   } catch (error) {
     console.log("config Failed" + error.message, error);
   }
